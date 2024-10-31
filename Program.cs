22237b683c31c0caf092147d423378c3df833401
Saruf Ratul <saruftoratul95@hotmail.com>
2024-10-31 18:07:12 +0600

    TBK & AM Open

--- Program.cs
+++ Program.cs
@@ -20,10 +20,10 @@ namespace FranchisorEXE
             //TBK
             //Saruf Ratul//
 
-            //FinancialRatioManagerTBK frTBK = new FinancialRatioManagerTBK();
-            //frTBK.Get_FinancialRatio_DataTBK();
+            FinancialRatioManagerTBK frTBK = new FinancialRatioManagerTBK();
+            frTBK.Get_FinancialRatio_DataTBK();
 
-            //Console.WriteLine("Financial Ratio Data TBK Loaded");
+            Console.WriteLine("Financial Ratio Data TBK Loaded");
 
             //AM
             //Saruf Ratul//
@@ -37,10 +37,10 @@ namespace FranchisorEXE
             //Demo
             //Saruf Ratul//
 
-            FinancialRatioManagerDemo frDemo = new FinancialRatioManagerDemo();
-            frDemo.Get_FinancialRatio_DataDemo();
+            //FinancialRatioManagerDemo frDemo = new FinancialRatioManagerDemo();
+            //frDemo.Get_FinancialRatio_DataDemo();
 
-            Console.WriteLine("Financial Ratio Data Demo Loaded");
+            //Console.WriteLine("Financial Ratio Data Demo Loaded");
         }
     }
 }
